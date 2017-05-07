37c06481dac90eae2745c86831cb9d2c4aae4592
Markku Korkeala <markku.korkeala@iki.fi>
2017-05-07 12:40:30 +0300

    Add: leiningen configuratation

---
+++ .lein/profiles.clj
@@ -0,0 +1,7 @@
+{:user {:plugins [[lein-midje "3.1.3"]
+                  [com.livingsocial/lein-dependency-check "0.2.1"] 
+                  [lein-bikeshed "0.4.1"]
+                  [jonase/eastwood "0.2.3"]
+                  [lein-kibit "0.1.4"]
+                  [lein-cloverage "1.0.6"]
+                  [lein-ancient "0.6.10"]]}}
